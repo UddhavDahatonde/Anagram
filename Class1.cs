fb0b587bbeb51cffba70a7e11def50459c10af06
Uddhav Dahatonde <dahatondeuddhava@gmail.com>
2022-10-04 08:57:50 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagram
+{
+    public class ToAnagram
+    {
+        public void To_find_anagram(string first, string second)
+        {
+            if (first.Length != second.Length)
+            {
+                Console.WriteLine("Both string are not anagram");
+            }
+            char[] first1 = first.ToLower().ToCharArray();
+            char[] second1 = second.ToLower().ToCharArray();
+            Array.Sort(first1);
+            Array.Sort(second1);
+            for (int i = 0; i < first1.Length; i++)
+            {
+                if (first1[i].ToString() != second1[i].ToString())
+                {
+                    Console.WriteLine("Both string are not anagram");
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Both string are anagram\n{first}  {second}");
+        }
+
+    }
+    public class Program
+    {
+        static void Main()
+        {
+            string firststring, secondstring;
+            Console.WriteLine("Enter the first string");
+            firststring = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Enter the second string");
+            secondstring = Convert.ToString(Console.ReadLine());
+            ToAnagram anagram = new ToAnagram();
+            anagram.To_find_anagram(firststring, secondstring);
+
+        }
+    }
+}
+
+
+
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User
+{
+    ////To find given word is anagram or not
+    //public class Anagram
+    //{
+    //    public bool Are_Anagram(string str1,string str2)
+    //    {
+    //        if(str1 .Length!=str2.Length)
+    //        {
+    //            return false;
+    //        }
+    //        char[] charstr1 = str1.ToLower() .ToCharArray();
+    //        char[] charstr2 = str2.ToLower() .ToCharArray();
+    //        Array.Sort(charstr1);
+    //        Array.Sort(charstr2);
+    //        for(int i=0; i < charstr1.Length; i++)
+    //        {
+    //            if(charstr1[i].ToString()!=charstr2[i].ToString())
+    //            {
+    //                return false;
+    //            }
+    //        }
+    //        return true;
+    //    }
+    //}
+    //internal class Program
+    //{
+
+    //    static void Main(string[] args)
+    //    {
+    //        string str1, str2;
+    //        Console.WriteLine("Enter the First String");
+    //        str1=Convert.ToString(Console.ReadLine());
+    //        Console.WriteLine("Enter the Second String");
+    //        str2 = Convert.ToString(Console.ReadLine());
+    //        Anagram anagram = new Anagram();
+    //        if(anagram.Are_Anagram(str1,str2)==true)
+    //        {
+    //            Console.WriteLine("Both String are Anagram");
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine("Both Strin are not Anagram");
+    //        }
+
+    //    }
+    //}
+}
